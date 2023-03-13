539d457250caefb8d05b1048551e0e122aa51ed7
tarifalhasan <tarifalhasan999@gmail.com>
2023-03-13 20:30:52 +0600

    complate project

--- src/components/Sponsors/Count.tsx
+++ src/components/Sponsors/Count.tsx
@@ -3,41 +3,25 @@ import React from 'react';
 const Count = () => {
   return (
     <section className="main-container ">
-      <div className="flex justify-between  mx-20 ">
-        <div className="md:ml-20 ">
+      <div className="flex justify-between  mx-10">
+        <div className="md:ml-20 text-center ">
           <h1 className="text-3xl md:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-[#7940dd] to-[#c33ba6] md:ml-10">
             100M
           </h1>
-          <span className="text-base pt-3 text-white font-normal">Supply</span>
+          <span className="text-base text-center pt-3 text-white font-normal">
+            Supply
+          </span>
         </div>
-        <div>
+        <div className="text-center">
           <h1 className="text-3xl md:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-[#7940dd] to-[#c33ba6]">
             200+
           </h1>
-          <span className="text-base pt-3  text-white fontnormalt">Data 2</span>
+          <span className="text-base pt-3 text-center  text-white font-normalt">
+            Data 2
+          </span>
         </div>
-        <div className="md:mr-20">
+        <div className="md:mr-20 text-center">
           <h1 className="text-3xl md:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-[#7940dd] to-[#c33ba6] md:mr-10">
-            1200+
-          </h1>
-          <span className="text-base pt-3 text-white font-normal">Data 2</span>
-        </div>
-      </div>
-      <div className="flex justify-between main-container mx-20">
-        <div className="mx-20">
-          <h1 className="text-3xl md:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-[#7940dd] to-[#c33ba6]">
-            100M
-          </h1>
-          <span className="text-base pt-3 text-white font-normal">Supply</span>
-        </div>
-        <div>
-          <h1 className="text-3xl md:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-[#7940dd] to-[#c33ba6]">
-            200+
-          </h1>
-          <span className="text-base pt-3  text-white fontnormalt">Data 2</span>
-        </div>
-        <div className="mr-20">
-          <h1 className="text-3xl md:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-[#7940dd] to-[#c33ba6]">
             1200+
           </h1>
           <span className="text-base pt-3 text-white font-normal">Data 2</span>
